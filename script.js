500ad6b3861c27b9317428f2211636bc49b63db8
Nitu170287 <108494260+Nitu170287@users.noreply.github.com>
2023-04-08 21:22:48 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function onSubmit(e){
+	alert("First Name: "+document.myForm.FirstName.value +
+		  "\nLast Name: "+document.myForm.LastName.value +
+		 "\nPhone Number: "+document.myForm.PhoneNumber.value +
+		 "\nEmail ID: "+document.myForm.EmailID.value );
+}
